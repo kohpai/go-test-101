e471ab5eeff9ee670fcca94aa7a0d1d5b9e6d196
Srikote Naewchampa <whcwhc78@gmail.com>
2019-05-02 14:28:04 +0800

    integration test

---
+++ strings/others_integration_test.go
@@ -0,0 +1,14 @@
+// +build integration
+
+package strings_test
+
+import (
+	"testing"
+	"time"
+)
+
+func TestIntegration(t *testing.T) {
+	// Long running integration test...
+	time.Sleep(3 * time.Second)
+	t.Log("running long integration test...")
+}
